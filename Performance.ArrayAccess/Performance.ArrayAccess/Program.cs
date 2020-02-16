31e0ceb1fc10990fc7d53bea54beaaada7d61752
Mike-EEE <michael@dragonspark.network>
2020-02-16 03:05:24 -0500

    Pushing results.

---
+++ Performance.ArrayAccess/Performance.ArrayAccess/Program.cs
@@ -0,0 +1,67 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Performance.ArrayAccess
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<Program>();
+		}
+
+		readonly int[] _source;
+
+		public Program() : this(Enumerable.Range(0, 1000).ToArray()) {}
+
+		public Program(int[] source) => _source = source;
+
+		[Benchmark]
+		public int Access()
+		{
+			var length = _source.Length;
+			var result = 0;
+			for (var i = 0u; i < length; i++)
+			{
+				result += _source[i];
+			}
+
+			return result;
+		}
+
+		[Benchmark(Baseline = true)]
+		public int Magic()
+		{
+			var     length = _source.Length;
+			var     result = 0;
+			for (var i = 0; i < length; i++)
+			{
+				result = _source.Access(i);
+			}
+
+			return result;
+		}
+	}
+
+	public static class Extensions
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static ref T Access<T>(this T[] @this, int index)
+		{
+			var     data = Unsafe.As<RawData>(@this);
+			ref var r0   = ref Unsafe.As<byte, T>(ref data.Data);
+			ref var r1   = ref Unsafe.Add(ref r0, index);
+			return ref r1;
+		}
+	}
+
+	sealed class RawData
+	{
+		public IntPtr Length;
+
+		public byte Data;
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 Checking out this nifty nugget here:
 https://twitter.com/SergioPedri/status/1228752877604265985
+
+
+```
+// * Summary *
+
+BenchmarkDotNet=v0.12.0, OS=Windows 10.0.18363
+AMD Ryzen 7 2700X, 1 CPU, 8 logical and 8 physical cores
+.NET Core SDK=3.1.101
+  [Host]     : .NET Core 3.1.1 (CoreCLR 4.700.19.60701, CoreFX 4.700.19.60801), X64 RyuJIT
+  DefaultJob : .NET Core 3.1.1 (CoreCLR 4.700.19.60701, CoreFX 4.700.19.60801), X64 RyuJIT
+
+
+| Method |     Mean |   Error |  StdDev | Ratio | RatioSD |
+|------- |---------:|--------:|--------:|------:|--------:|
+| Access | 462.4 ns | 7.14 ns | 6.68 ns |  1.28 |    0.03 |
+|  Magic | 361.5 ns | 6.20 ns | 5.80 ns |  1.00 |    0.00 |
+```
